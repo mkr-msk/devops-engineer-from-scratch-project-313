11de2f86eb7724d41de8c6bc47890fd5071ba5f1
mkr-msk <mkr_msk@mail.ru>
2026-01-31 09:19:14 +0300

    add import sqlmodel

--- alembic/versions/d63173572584_create_link_table.py
+++ alembic/versions/d63173572584_create_link_table.py
@@ -1,18 +1,19 @@
 """Create link table
 
-Revision ID: 391267356e2c
+Revision ID: d63173572584
 Revises: 
-Create Date: 2026-01-30 23:57:19.716673
+Create Date: 2026-01-31 09:17:05.744539
 
 """
 from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
+import sqlmodel
 
 
 # revision identifiers, used by Alembic.
-revision: str = '391267356e2c'
+revision: str = 'd63173572584'
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
