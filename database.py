399b1b279d7210144510b1644b5d50c16a1a250e
mkr-msk <mkr_msk@mail.ru>
2026-02-04 18:57:01 +0300

    update versions

--- database.py
+++ database.py
@@ -9,4 +9,7 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 if not DATABASE_URL:
   raise ValueError('DATABASE_URL is not set')
 
+if DATABASE_URL.startswith('postgresql://'):
+  DATABASE_URL = DATABASE_URL.replace('postgresql://', 'postgresql+psycopg://', 1)
+
 engine = create_engine(DATABASE_URL, echo=False)
