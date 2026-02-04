e0d0e02d62d66c31332c82e5843a6034b0b5c2fc
mkr-msk <mkr_msk@mail.ru>
2026-02-04 19:01:57 +0300

    fix database.py

--- database.py
+++ database.py
@@ -9,7 +9,9 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 if not DATABASE_URL:
   raise ValueError('DATABASE_URL is not set')
 
-if DATABASE_URL.startswith('postgresql://'):
+if DATABASE_URL.startswith('postgres://'):
+  DATABASE_URL = DATABASE_URL.replace('postgres://', 'postgresql+psycopg://', 1)
+elif DATABASE_URL.startswith('postgresql://'):
   DATABASE_URL = DATABASE_URL.replace('postgresql://', 'postgresql+psycopg://', 1)
 
 engine = create_engine(DATABASE_URL, echo=False)
